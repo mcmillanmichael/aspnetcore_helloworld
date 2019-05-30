2e644cb5fcf114d22e48799e465be874aec6e916
mike.mcmillan <mike.mcmillan@airport-systems.com>
2019-05-30 12:58:47 +0100

    Switched from card-deck to row/col-sm for layouts. Moved font-awesome under wwwroot\lib\, and fixed the css links for !Development. Removed hello-world wording throughout. Added Years/Months of dev on index.

--- aspnetcore_helloworld/Pages/Index.cshtml.cs
+++ aspnetcore_helloworld/Pages/Index.cshtml.cs
@@ -10,12 +10,24 @@ namespace aspnetcore_helloworld.Pages
     public class IndexModel : PageModel
     {
         public int DevTimeYears { get; private set; }
+        public int DevTimeMonths { get; private set; }
+
+        public string ProfileImageSource { get; private set; }
 
         public void OnGet()
         {
-            var firstJobDate = new DateTime(2008, 06, 01);
+            var firstJobDate = new DateTime(2008, 06, 09);
             var devDuration = DateTime.UtcNow - firstJobDate;
-            DevTimeYears = (int)(devDuration.Days / 365.25);
+            var span = DateTime.MinValue + devDuration;
+            DevTimeYears = span.Year - 1;
+            DevTimeMonths = span.Month - 1;
+
+#if DEBUG
+            // just so I don't have to keep looking at myself whilst debugging...
+            ProfileImageSource = "img/default-profile.jpg";
+#else
+            ProfileImageSource = "img/mikemcmillan.jpg";
+#endif
         }
     }
 }
